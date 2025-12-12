807a60b4dd44b0271aa06b4ba277082b0a44502e
Keshav_cyber <patidarhariom134@gmail.com>
2025-12-12 15:41:33 +0530

    pro

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -6,41 +6,28 @@ const redis = require("redis");
 
 const { promisify } = require("util");
 
-//Connect to redis
-const redisClient = redis.createClient(
-    18251,
-    "redis-18251.c301.ap-south-1-1.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
-  );
-  redisClient.auth("g0Kd6m7qXMjNbGaLYSCpG3vPAxBRb2GZ", function (err) {
-    if (err) throw err;
-  });
-  
-  redisClient.on("connect", async function () {
-    console.log("Connected to Redis..");
-  });
-
-const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
-const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const createShortUrl = async function (req, res) {
     try {
+        
         let url = req.body.longUrl
+        console.log(url);
+
         if (!validation.isValidField(url)) return res.status(400).send({ status: false, msg: 'url cannot be empty' })
         if (!validation.isValidUrl(url)) return res.status(400).send({ status: false, msg: 'enter valid url' })
-
         if (!validUrl.isUri(url.toString())) {
             return res.status(400).send({ status: false, msg: "enter valid url" })
         }
+
         let urlcheck = await urlModel.findOne({ longUrl: url }).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 0 })
         if (urlcheck) return res.status(200).send({ status: true, data: urlcheck })
-        
-        
+               
         let urlCode = shortId.generate()
         req.body.urlCode = urlCode
         
         let shortUrl = "http://localhost:3000/" + urlCode
         req.body.shortUrl = shortUrl
+        
         let createdShortUrl = await urlModel.create(req.body)
         let { createdAt, updatedAt, __v, _id, ...result } = createdShortUrl._doc
 
@@ -53,19 +40,13 @@ const createShortUrl = async function (req, res) {
 
 const getUrl = async function (req, res) {
     try {
-        let reqParams = req.params.urlCode
-        let cacheShortUrl = await GET_ASYNC(`${reqParams}`)
-        if(cacheShortUrl){
-            res.status(302).redirect(cacheShortUrl)
-        }
-        else{
-        
-        if(!shortId.isValid(reqParams)) return res.status(400).send({ status: false, msg: 'invalid url code' }) 
-        let findUrlCode = await urlModel.findOne({ urlCode: reqParams }).select({ longUrl: 1, _id: 0 })
-        if(findUrlCode == null) return res.status(404).send({ status: false, msg: 'Url not found' })
-        await SET_ASYNC(`${reqParams}`, findUrlCode.longUrl)
+        let urlCode = req.params.urlCode      
+        if(!shortId.isValid(urlCode)) return res.status(400).send({ status: false, msg: 'invalid url code' }) 
+        let findUrlCode = await urlModel.findOne({ urlCode: urlCode }).select({ longUrl: 1, _id: 0 })
+        if (!findUrlCode)return res.status(404).send({ status: false, msg: 'Url not found' })
+            console.log(findUrlCode.longUrl);
         return res.status(302).redirect( findUrlCode.longUrl )
-        }
+        
     }
     catch (error) {
         res.status(500).send({ msg: error.message })
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-
+const cors = require('cors');
+const mongoose = require('mongoose')
 const app = express();
+require('dotenv').config();
 
 
 app.use(bodyParser.json());
+app.use(cors({
+  origin:'https://shortlinxfront.vercel.app/'// Replace with your actual frontend port
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
-const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://Keshav-cyber:7LizqrsG6tL39fuT@cluster0.ohm0bak.mongodb.net/group25Database?retryWrites=true&w=majority", { useNewUrlParser: true })
+console.log('mongodb+srv://'+process.env.MONGOUSER+':'+process.env.MONGOPASSWORD+'@cluster0.raflcmc.mongodb.net/?appName=Cluster0')
+
+mongoose.connect('mongodb+srv://'+process.env.MONGOUSER+':'+process.env.MONGOPASSWORD+'@cluster0.raflcmc.mongodb.net/?appName=Cluster0', { useNewUrlParser: true })
     .then(() => console.log('mongodb running on cluster ✔'))
     .catch(err => console.log(err))
 
